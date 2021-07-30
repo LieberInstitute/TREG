a1b758e52d8b47626a2e90a00c92a0a1b573c248
Louise Huuki <lahuuki@gmail.com>
2021-07-30 12:16:49 -0400

    Build zero test data

---
+++ data-raw/sce_zero_test.R
@@ -0,0 +1,34 @@
+## code to prepare `sce_zero_test` dataset goes here
+
+library(SingleCellExperiment)
+
+pd <- S4Vectors::DataFrame(
+  donor = rep(c("D1", "D2"), each = 50),
+  cellType = rep(rep(c("A", "B"), each = 25), 2),
+  region = rep(rep(c("Front", "Back"), each = 10), 5)
+)
+rownames(pd) <- paste0("S", stringr::str_pad(1:nrow(pd), width = nchar(nrow(pd)), pad = "0"))
+
+combo <- paste0(pd$cellType, "_", pd$region)
+(tc <- table(combo))
+# A_Back A_Front  B_Back B_Front
+#     25      25      25      25
+
+
+counts <- matrix(data = c(rep(0, 100),
+                          rep(c(1,0), 50),
+                          sample(1:100, 100),
+                          c(rep(1, 25), rep(0, 25), rep(1, 25), rep(0,25)),
+                          c(rbinom(25, 1, 0.2),rbinom(25, 1, 0.4),rbinom(25, 1, 0.6),rbinom(25, 1, 0.8))),
+                 ncol = 100,
+                 dimnames = list(c("g100","g50","g0","gOffOn", 'gVar'), rownames(pd)),
+                 byrow = TRUE)
+
+sce_zero_test <- SingleCellExperiment(list(counts = counts),
+                               colData = pd
+)
+
+assays(sce_zero_test)$logcounts <- log(assays(sce_zero_test)$counts+1)
+
+
+usethis::use_data(sce_zero_test, overwrite = TRUE)
