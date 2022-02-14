7a954aedc592d174591bf3284bf80e52e73f610c
Louise Huuki <lahuuki@gmail.com>
2022-02-14 16:16:46 -0500

    Add data docs

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Toy sce
+#'
+#' A dataset with a range of proportion of zeros across groups for differnt genes
+#'
+#' @format A SingleCellExperiment Object with 5 rows and 100 columns
+"sce_zero_test"
